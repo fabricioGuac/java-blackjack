09fe4364fc980761c4c566a0aa4fc9850ec68f26
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-10-06 16:03:52 -0700

    downloads the card images folder into the project and delves in the card class

---
+++ src/javaBlackjack/BlackjackMain.java
@@ -0,0 +1,7 @@
+package javaBlackjack;
+
+public class BlackjackMain {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello world");
+	}
+}
---
+++ src/javaBlackjack/Card.java
@@ -0,0 +1,5 @@
+package javaBlackjack;
+
+public class Card {
+
+}
